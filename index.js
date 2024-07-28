464d5b8d832b32d51c721b8375009ede45bc4436
silas <silasgebhart12@gmail.com>
2024-07-28 15:44:43 -0500

    Testing function(isAlive)

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ if (age < 21) {
 }
 
 // BlackJack game equations
-let firstCard = 6;
-let secondCard = 9;
-let sum = firstCard + secondCard;
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard + 4;
 let hasBlackJack = false;
+let isAlive = true;
 
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-  console.log("Blackjack!");
+  console.log("Wohoo! You've got Blackjack! 🥳");
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game!");
+  console.log("You're out of the game! 😭");
+  isAlive = false;
 }
+
+console.log(isAlive);

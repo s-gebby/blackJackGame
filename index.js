01fcd40b13755a87972b0470105f0ac7c07b5772
silas <silasgebhart12@gmail.com>
2024-08-04 17:39:00 -0500

    Adding in fake dollar amount

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ if (age < 21) {
   console.log("Welcome!");
 }
 
+let player = {
+  name: "Per",
+  chips: 1000,
+};
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -15,6 +19,8 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
   let randomNumer = Math.floor(Math.random() * 13) + 1;

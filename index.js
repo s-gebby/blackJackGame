226825bb58960548cc028e9b71408325ba322a5b
silas <silasgebhart12@gmail.com>
2024-08-04 17:21:55 -0500

    Not allowing new card after you lose

--- index.js
+++ index.js
@@ -7,16 +7,34 @@ if (age < 21) {
   console.log("Welcome!");
 }
 
-let firstCard = 1;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+  let randomNumer = Math.floor(Math.random() * 13) + 1;
+  if (randomNumer > 10) {
+    return 10;
+  } else if (randomNumer === 1) {
+    return 11;
+  } else {
+    return randomNumer;
+  }
+}
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
 
 function renderGame() {
   cardsEl.textContent = "Cards: ";
@@ -37,12 +55,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 6;
-  sum += card;
-  cards.push(card);
-  renderGame();
-}
-
-function startGame() {
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
